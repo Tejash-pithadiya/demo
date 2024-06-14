ac3033f0d1658bb191480e5030541dea5e612786
TejashPithadiya <tejashpithadiya5951@gmail.com>
2024-06-14 15:10:45 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,63 @@
 
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import Home from './componet/Home'
-import Menu from './componet/Menu'
-import Blog from './componet/Blog'
-import Aboutus from './componet/About-us'
-import Contactus from './componet/Contact-us'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Navbar from './componet/Navbar'
+// import './App.css'
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import Home from './componet/Home'
+// import Menu from './componet/Menu'
+// import Blog from './componet/Blog'
+// import Aboutus from './componet/About-us'
+// import Contactus from './componet/Contact-us'
+// import { BrowserRouter,Route,Routes } from 'react-router-dom'
+// import Navbar from './componet/Navbar'
 
 
-function App() {
+// function App() {
  
 
+//   return (
+//     <>
+//     <BrowserRouter basename='/'>
+//       <Navbar/> 
+//       <Routes>
+//       <Route path='/' element={<Home/>}/>
+//       <Route path='/about-us' element={<Aboutus/>}/>
+//       <Route path='/menu' element={<Menu/>}/>
+//       <Route path='/contact-us' element={<Contactus/>}/>
+//       <Route path='/blog' element={<Blog/>}/>
+//       </Routes>
+//     </BrowserRouter>
+    
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from './componet/Home';
+import Menu from './componet/Menu';
+import Blog from './componet/Blog';
+import Aboutus from './componet/About-us';
+import Contactus from './componet/Contact-us';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './componet/Navbar';
+
+function App() {
   return (
     <>
-    <BrowserRouter basename='/'>
-      <Navbar/> 
-      <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about-us' element={<Aboutus/>}/>
-      <Route path='/menu' element={<Menu/>}/>
-      <Route path='/contact-us' element={<Contactus/>}/>
-      <Route path='/blog' element={<Blog/>}/>
-      </Routes>
-    </BrowserRouter>
-    
+      <Router basename="/demo">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about-us' element={<Aboutus />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path='/contact-us' element={<Contactus />} />
+          <Route path='/blog' element={<Blog />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
